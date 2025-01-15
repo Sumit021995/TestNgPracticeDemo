8d516ca9850200df1d0fd4002ba520f8ce39fa04
Sumit021995 <kaku.saurav@gmail.com>
2025-01-16 00:53:03 +0530

    new commit

--- TestNgPracticeDemo/src/test/java/testPractice/TestNgPriorityAndDependsOnMethods.java
+++ TestNgPracticeDemo/src/test/java/testPractice/TestNgPriorityAndDependsOnMethods.java
@@ -3,16 +3,23 @@ package testPractice;
 import org.testng.annotations.Test;
 
 public class TestNgPriorityAndDependsOnMethods {
-	@Test(priority=1)
+	
+	@Test(priority=1,invocationCount=3)
 	public void createUser()
 	{
 		System.out.println("User Created");
 	}
-	@Test(priority=2,dependsOnMethods= {"createuser"})
+	@Test(priority=2,dependsOnMethods={"createUser"})
 	public void updateUser()
 	{
 		System.out.println("User Updated");
 	}
+	@Test(priority=3,dependsOnMethods={"createUser"})
+	public void deleteUser()
+	{
+		System.out.println("User Deleted");
+	}
+	
 //	@Test(priority=3,dependsOnMethods= {"createuse"}) // it will give TestNGException because method name is incorrect
 //	public void deleteUser()
 //	{
