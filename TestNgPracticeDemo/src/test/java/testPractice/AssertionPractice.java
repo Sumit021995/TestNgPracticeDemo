b6a3ad4a43956f304f5cfc1d06cc461f06ad91c5
Sumit021995 <kaku.saurav@gmail.com>
2025-01-16 22:07:32 +0530

    new commit

--- TestNgPracticeDemo/src/test/java/testPractice/AssertionPractice.java
+++ TestNgPracticeDemo/src/test/java/testPractice/AssertionPractice.java
@@ -4,13 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AssertionPractice {
-	@Test
+	@Test(priority=1)
 	public void strictLevelHardAssertion()
 	{
 		String actualName="Sumit Saurav";
 		String expectedName="Sumit";
 		Assert.assertEquals(actualName,expectedName);
 		Assert.assertEquals(actualName.equals(expectedName),true);
+		System.out.println("Validation Ended");;
+	}
+	@Test(priority=2)
+	public void containsLevelHardAssertion()
+	{
+		String actualName="don";
+		String expectedName="London";
+		Assert.assertTrue(actualName.contains(expectedName));
+//		Assert.assertEquals(actualName.equals(expectedName),true);
 		System.out.println("Validation Ended");;
 	}
 }
