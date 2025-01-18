bfcb5f5789953196b570ef1305e140a0729c2701
Sumit021995 <kaku.saurav@gmail.com>
2025-01-18 23:50:58 +0530

    new commit

--- TestNgPracticeDemo/src/test/java/testPractice/AnnotationsTestNG.java
+++ TestNgPracticeDemo/src/test/java/testPractice/AnnotationsTestNG.java
@@ -1,5 +1,12 @@
 package testPractice;
 
-public class AnnotationsTestNG {
+import org.testng.annotations.Test;
 
+public class AnnotationsTestNG {
+	
+	@Test(priority=1)
+	public void addUser()
+	{
+		
+	}
 }
