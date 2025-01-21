8ba12f62f856267ee451fbec973cc1951b816c1d
Sumit021995 <kaku.saurav@gmail.com>
2025-01-21 23:47:38 +0530

    new commit

--- TestNgPracticeDemo/src/test/java/testPractice/BaseClass.java
+++ TestNgPracticeDemo/src/test/java/testPractice/BaseClass.java
@@ -1,5 +1,15 @@
 package testPractice;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
+public class BaseClass {
+	@BeforeSuite
+	@BeforeClass
+	public void loginToApp()
+	{
+		WebDriver driver = new ChromeDriver();
+	}
 }
